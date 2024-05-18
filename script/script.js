74e0fd0f91d82610f90b8482bc9df22b2133e2c1
dbeee <dbeee.acharya@gmail.com>
2024-05-18 13:23:05 +0545

    Add cards and animation on scroll

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+// Animation for slide in when scrolling 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
