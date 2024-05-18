334340ed460ab4ad43be91dd8cf62cf39fefc187
dbeee <dbeee.acharya@gmail.com>
2024-05-18 16:40:21 +0545

    Complete why choose us section two with animations

--- script/script.js
+++ script/script.js
@@ -10,5 +10,18 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
+const observerUp = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("go-up")
+        } else {
+            entry.target.classList.remove("go-up")
+        }
+    });
+});
+
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElementsDown = document.querySelectorAll(".hidden-down");
+hiddenElementsDown.forEach((el) => observerUp.observe(el));
